d2b19c893b7f8c63154c5b5ff2c419ffdc8baa0c
James Halliday <mail@substack.net>
2011-10-03 03:49:23 -0700

    passing mock test with package.json

--- test/mock.js
+++ test/mock.js
@@ -12,7 +12,7 @@ exports.mock = function () {
             isFile : function (file) {
                 return files.hasOwnProperty(file)
             },
-            readFile : function (file) {
+            readFileSync : function (file) {
                 return files[file]
             }
         }
@@ -31,4 +31,30 @@ exports.mock = function () {
     assert.throws(function () {
         resolve.sync('baz', opts('/foo/bar'));
     });
+};
+
+exports.mockPackage = function () {
+    var files = {
+        '/foo/node_modules/bar/baz.js' : 'beep',
+        '/foo/node_modules/bar/package.json' : JSON.stringify({
+            main : './baz.js'
+        })
+    };
+    
+    function opts (basedir) {
+        return {
+            basedir : basedir,
+            isFile : function (file) {
+                return files.hasOwnProperty(file)
+            },
+            readFileSync : function (file) {
+                return files[file]
+            }
+        }
+    }
+    
+    assert.equal(
+        resolve.sync('bar', opts('/foo')),
+        '/foo/node_modules/bar/baz.js'
+    );
 };
