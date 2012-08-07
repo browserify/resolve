074782a0525fc5e53a587fe7f78d20e10c57b72a
schinken <schinken@hackerspace-bamberg.de>
2012-08-07 15:20:17 +0200

    Use obj, not just declare it

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ exports.sync = function (x, opts) {
     if (!opts) opts = {};
     var isFile = opts.isFile || function (file) {
         var obj = fs.existsSync ? fs : path;
-        return path.existsSync(file) && fs.statSync(file).isFile()
+        return obj.existsSync(file) && fs.statSync(file).isFile()
     };
     var readFileSync = opts.readFileSync || fs.readFileSync;
     
