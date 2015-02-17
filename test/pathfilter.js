7f0a3f1545f4b53f1bdd099b67561f9516693325
James Halliday <mail@substack.net>
2015-02-17 00:35:40 -0800

    move pathfilter files into their own dir

--- test/pathfilter.js
+++ test/pathfilter.js
@@ -3,8 +3,8 @@ var resolve = require('../');
 
 test('#62: deep module references and the pathFilter', function(t){
 	t.plan(6);
-	
-	var resolverDir = __dirname + '/resolver/deep_ref';
+    
+	var resolverDir = __dirname + '/pathfilter/deep_ref';
 	var pathFilter = function(pkg, x, remainder){
 		t.equal(pkg.version, "1.2.3");
 		t.equal(x, resolverDir + '/node_modules/deep/ref');
