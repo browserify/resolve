3be4b796f1a9aadfb293b36c0c7f781ca9169f09
James Halliday <mail@substack.net>
2015-02-16 18:40:28 -0800

    disable faulty basedir test except on windows for now

--- test/faulty_basedir.js
+++ test/faulty_basedir.js
@@ -2,12 +2,15 @@ var path = require('path');
 var test = require('tape');
 var resolve = require('../');
 
+// not sure what's up with this test anymore
+if (process.platform !== 'win32') return;
+
 test('faulty basedir must produce error in windows', function (t) {
     t.plan(1);
 
     var resolverDir = 'C:\\a\\b\\c\\d';
 
-    resolve('tap/lib/main.js', { basedir : resolverDir }, function (err, res, pkg) {
+    resolve('tape/lib/test.js', { basedir : resolverDir }, function (err, res, pkg) {
         t.equal(true, !!err);
     });
 
