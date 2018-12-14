765b3cb6627d2592f2c97e1416bf8ee2302d4b6b
Jordan Harband <ljharb@gmail.com>
2018-12-14 14:22:05 -0800

    [Fix] `sync`/`async`: fix `preserveSymlinks` option
    Fixes #177.

--- test/node_path.js
+++ test/node_path.js
@@ -1,3 +1,4 @@
+var fs = require('fs');
 var path = require('path');
 var test = require('tape');
 var resolve = require('../');
@@ -5,12 +6,26 @@ var resolve = require('../');
 test('$NODE_PATH', function (t) {
     t.plan(8);
 
+    var isDir = function (dir, cb) {
+        if (dir === '/node_path' || dir === 'node_path/x') {
+            return cb(null, true);
+        }
+        fs.stat(dir, function (err, stat) {
+            if (!err) {
+                return cb(null, stat.isDirectory());
+            }
+            if (err.code === 'ENOENT' || err.code === 'ENOTDIR') return cb(null, false);
+            return cb(err);
+        });
+    };
+
     resolve('aaa', {
         paths: [
             path.join(__dirname, '/node_path/x'),
             path.join(__dirname, '/node_path/y')
         ],
-        basedir: __dirname
+        basedir: __dirname,
+        isDirectory: isDir
     }, function (err, res) {
         t.error(err);
         t.equal(res, path.join(__dirname, '/node_path/x/aaa/index.js'), 'aaa resolves');
@@ -21,7 +36,8 @@ test('$NODE_PATH', function (t) {
             path.join(__dirname, '/node_path/x'),
             path.join(__dirname, '/node_path/y')
         ],
-        basedir: __dirname
+        basedir: __dirname,
+        isDirectory: isDir
     }, function (err, res) {
         t.error(err);
         t.equal(res, path.join(__dirname, '/node_path/y/bbb/index.js'), 'bbb resolves');
@@ -32,21 +48,20 @@ test('$NODE_PATH', function (t) {
             path.join(__dirname, '/node_path/x'),
             path.join(__dirname, '/node_path/y')
         ],
-        basedir: __dirname
+        basedir: __dirname,
+        isDirectory: isDir
     }, function (err, res) {
         t.error(err);
         t.equal(res, path.join(__dirname, '/node_path/x/ccc/index.js'), 'ccc resolves');
     });
 
-    /*
-     * ensure that relative paths still resolve against the
-     * regular `node_modules` correctly
-     */
+    // ensure that relative paths still resolve against the regular `node_modules` correctly
     resolve('tap', {
         paths: [
             'node_path'
         ],
-        basedir: 'node_path/x'
+        basedir: path.join(__dirname, 'node_path/x'),
+        isDirectory: isDir
     }, function (err, res) {
         var root = require('tap/package.json').main;
         t.error(err);
--- test/resolver.js
+++ test/resolver.js
@@ -364,7 +364,7 @@ test('async dot slash main', function (t) {
 });
 
 test('not a directory', function (t) {
-    t.plan(5);
+    t.plan(6);
     var path = './foo';
     resolve(path, { basedir: __filename }, function (err, res, pkg) {
         t.ok(err, 'a non-directory errors');
@@ -372,7 +372,8 @@ test('not a directory', function (t) {
         t.equal(res, undefined);
         t.equal(pkg, undefined);
 
-        t.equal(err && err.message, 'Cannot find module \'' + path + "' from '" + __filename + "'");
+        t.equal(err && err.message, 'Cannot find module \'' + path + '\' from \'' + __filename + '\'');
+        t.equal(err && err.code, 'MODULE_NOT_FOUND');
     });
 });
 
