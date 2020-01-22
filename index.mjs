4f43e04243a2d317a41c4084ce4e3924c8546805
Jordan Harband <ljharb@gmail.com>
2020-01-21 17:25:38 -0800

    [Breaking] add ESM wrapper to "exports"; change paths to match

---
+++ index.mjs
@@ -0,0 +1,8 @@
+import async from 'resolve/async';
+import sync from 'resolve/sync';
+import core from 'resolve/core';
+import isCore from 'resolve/isCore';
+
+Object.freeze(isCore);
+
+export { async, sync, core, isCore };
